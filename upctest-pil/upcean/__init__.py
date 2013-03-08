5e3908e358c494190fc1ed38cb06ab23d3cddd40
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2013-03-07 19:47:19 -0600

    Added more shortcuts functions.

--- upctest-pil/upcean/__init__.py
+++ upctest-pil/upcean/__init__.py
@@ -13,7 +13,7 @@
     Copyright 2011-2012 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2011-2012 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: __init__.py - Last Update: 10/04/2012 Ver. 2.0.0 - Author: cooldude2k $
+    $FileInfo: __init__.py - Last Update: 03/07/2013 Ver. 2.0.0 - Author: cooldude2k $
 '''
 
 from __future__ import division;
@@ -103,4 +103,80 @@ def create_barcode(upc,outfile="./barcode.png",resize=1,hideinfo=(False, False, 
    return create_itf14(upc,outfile,resize,hideinfo,barheight);
   if(supplement!=None):
    return create_itf14(upc+" "+supplement,outfile,resize,hideinfo,barheight);
+ return False;
+def validate_upc(upc,return_check=False):
+ upc = str(upc);
+ if(len(upc)==8): 
+  return validate_upce(upc,return_check);
+ if(len(upc)==12): 
+  return validate_upca(upc,return_check);
+ return False;
+def fix_upc_checksum(upc):
+ upc = str(upc);
+ if(len(upc)==7): 
+  return upc+str(validate_upce(upc,True));
+ if(len(upc)==11): 
+  return upc+str(validate_upca(upc,True));
+ return False;
+def create_upc(upc,outfile="./barcode.png",resize=1,hideinfo=(False, False, False),barheight=(47, 53)):
+ upc = str(upc);
+ if(not re.findall("^([0-9]*[\.]?[0-9])", str(resize)) or int(resize) < 1):
+  resize = 1;
+ upc_pieces = None; supplement = None;
+ if(re.findall("([0-9]+)([ |\|]{1})([0-9]{2})$", upc)):
+  upc_pieces = re.findall("([0-9]+)([ |\|]{1})([0-9]{2})$", upc);
+  upc_pieces = upc_pieces[0];
+  upc = upc_pieces[0]; supplement = upc_pieces[2];
+ if(re.findall("([0-9]+)([ |\|]{1})([0-9]{5})$", upc)):
+  upc_pieces = re.findall("([0-9]+)([ |\|]{1})([0-9]{5})$", upc);
+  upc_pieces = upc_pieces[0];
+  upc = upc_pieces[0]; supplement = upc_pieces[2];
+ if(len(upc)==7 or len(upc)==8):
+  if(supplement==None):
+   return create_upce(upc,outfile,resize,hideinfo,barheight);
+  if(supplement!=None):
+   return create_upce(upc+" "+supplement,outfile,resize,hideinfo,barheight);
+ if(len(upc)==11 or len(upc)==12):
+  if(supplement==None):
+   return create_upca(upc,outfile,resize,hideinfo,barheight);
+  if(supplement!=None):
+   return create_upca(upc+" "+supplement,outfile,resize,hideinfo,barheight);
+ return False;
+def validate_ean(upc,return_check=False):
+ upc = str(upc);
+ if(len(upc)==8): 
+  return validate_ean8(upc,return_check);
+ if(len(upc)==13): 
+  return validate_ean13(upc,return_check);
+ return False;
+def fix_ean_checksum(upc):
+ upc = str(upc);
+ if(len(upc)==7): 
+  return upc+str(validate_ean8(upc,True));
+ if(len(upc)==12): 
+  return upc+str(validate_ean13(upc,True));
+ return False;
+def create_ean(upc,outfile="./barcode.png",resize=1,hideinfo=(False, False, False),barheight=(47, 53)):
+ upc = str(upc);
+ if(not re.findall("^([0-9]*[\.]?[0-9])", str(resize)) or int(resize) < 1):
+  resize = 1;
+ upc_pieces = None; supplement = None;
+ if(re.findall("([0-9]+)([ |\|]{1})([0-9]{2})$", upc)):
+  upc_pieces = re.findall("([0-9]+)([ |\|]{1})([0-9]{2})$", upc);
+  upc_pieces = upc_pieces[0];
+  upc = upc_pieces[0]; supplement = upc_pieces[2];
+ if(re.findall("([0-9]+)([ |\|]{1})([0-9]{5})$", upc)):
+  upc_pieces = re.findall("([0-9]+)([ |\|]{1})([0-9]{5})$", upc);
+  upc_pieces = upc_pieces[0];
+  upc = upc_pieces[0]; supplement = upc_pieces[2];
+ if(len(upc)==7 or len(upc)==8):
+  if(supplement==None):
+   return create_ean8(upc,outfile,resize,hideinfo,barheight);
+  if(supplement!=None):
+   return create_ean8(upc+" "+supplement,outfile,resize,hideinfo,barheight);
+ if(len(upc)==12 or len(upc)==13): 
+  if(supplement==None):
+   return create_ean13(upc,outfile,resize,hideinfo,barheight);
+  if(supplement!=None):
+   return create_ean13(upc+" "+supplement,outfile,resize,hideinfo,barheight);
  return False;
