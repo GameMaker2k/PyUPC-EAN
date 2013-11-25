d475c2b14c541c295210bfce3d1656620d6fc477
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2013-11-25 00:25:55 -0600

    Small update.

--- httpd.py
+++ httpd.py
@@ -13,7 +13,7 @@
     Copyright 2011-2013 Game Maker 2k - http://intdb.sourceforge.net/
     Copyright 2011-2013 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: httpd.py - Last Update: 11/24/2013 Ver. 2.5.0 RC 2  - Author: cooldude2k $
+    $FileInfo: httpd.py - Last Update: 11/25/2013 Ver. 2.5.0 RC 3  - Author: cooldude2k $
 '''
 
 import tempfile, uuid, re, os, sys, cherrypy, upcean, StringIO, argparse, time, datetime;
