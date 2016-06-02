7071ec3759de49c045e3cde1c720e3b0e5b19f21
Kazuki Przyborowski <kazuki.przyborowski@gmail.com>
2016-06-02 10:15:17 -0500

    Making new release version 2.7.14 RC 1.

--- upcean/versioninfo.py
+++ upcean/versioninfo.py
@@ -10,7 +10,7 @@
     Copyright 2011-2016 Game Maker 2k - https://github.com/GameMaker2k
     Copyright 2011-2016 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: versioninfo.py - Last Update: 4/20/2016 Ver. 2.7.13 RC 1 - Author: cooldude2k $
+    $FileInfo: versioninfo.py - Last Update: 6/1/2016 Ver. 2.7.14 RC 1 - Author: cooldude2k $
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals;
@@ -66,7 +66,7 @@ __email__ = "kazuki.przyborowski@gmail.com";
 __status__ = "Production";
 __project__ = "PyUPC-EAN";
 __project_url__ = "https://pypi.python.org/pypi/PyUPC-EAN";
-__version_info__ = (2, 7, 13, "RC 1", 1);
+__version_info__ = (2, 7, 14, "RC 1", 1);
 __build_time__ = {"timestamp": None, "year": None, "month": None, "day": None, "hour": None, "minute": None, "second": None};
 __build_time_utc__ = {"timestamp": None, "year": None, "month": None, "day": None, "hour": None, "minute": None, "second": None};
 __build_python_info__ = {"python_branch": None, "python_build": None, "python_compiler": None, "python_implementation": None, "python_revision": None, "python_version": None, "python_version_tuple": None, "release": None, "system": None, "uname": None, "machine": None, "node": None, "platform": None, "processor": None, "version": None, "java_ver": None, "win32_ver": None, "mac_ver": None, "linux_distribution": None, "libc_ver": None};
@@ -88,7 +88,7 @@ def version_info():
   return {"major": __version_info__[0], "minor": __version_info__[1], "build": __version_info__[2], "release": __version_info__[3]};
  if(__version_info__[3] is None):
   return {"major": __version_info__[0], "minor": __version_info__[1], "build": __version_info__[2], "release": None};
-__version_date_info__ = (2016, 4, 20, "RC 1", 1);
+__version_date_info__ = (2016, 6, 1, "RC 1", 1);
 def version_date():
  if(__version_date_info__[3] is not None):
   return {"year":__version_date_info__[0], "month": __version_date_info__[1], "day": __version_date_info__[2], "release": __version_date_info__[3]};
