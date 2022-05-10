e0641bc0aea1e2ac36bec60c904e15636d8df424
Joshua Thomas Przyborowski <joshua.przyborowski@gmail.com>
2022-05-10 17:49:26 -0500

    Update getprefix.py

--- upcean/getprefix/getprefix.py
+++ upcean/getprefix/getprefix.py
@@ -407,9 +407,9 @@ def get_goodwill_upca_barcode_info(upc, infotype=None):
  else:
   gw_item_color = None;
  price_matches = re.findall("^(\d{3})(\d{2})", upc_matches[1]);
- price_alt = str(price_matches[0].lstrip('0'))+price_matches[0]);
+ price_alt = str(price_matches[0].lstrip('0'))+price_matches[0];
  formated_price = price_matches[0]+"."+price_matches[0]);
- formated_price_alt = str(price_matches[0].lstrip('0'))+"."+price_matches[0]);
+ formated_price_alt = str(price_matches[0].lstrip('0'))+"."+price_matches[0];
  product = {'numbersystem': str(4), 'code': upc_matches[0], 'price': upc_matches[1], 'pricend': price_alt, 'pricewd': formated_price, 'pricewdnz': formated_price_alt, 'type': gw_item_type, 'tagcolor': gw_item_color, 'checkdigit': upc_matches[2]};
  if(infotype is None):
   return product;
