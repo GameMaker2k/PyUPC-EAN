6260a8e3488539bcfa5ca6a470987c51be0824fb
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2022-05-27 14:52:36 -0500

    Add files via upload

---
+++ examples/upc-json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+'''
+     Code From: https://github.com/fizyk20/python-barcode
+     Code From: https://github.com/fizyk20/python-barcode/blob/master/example.py
+         $FileInfo: upc-example.py - Last Update: 12/3/2019 Ver. 2.7.19 RC 1  - Author: fizyk20 $
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals;
+import upcean;
+
+upcean.barcodes.create_barcode_from_json_file("./xml/barcodes.json");
