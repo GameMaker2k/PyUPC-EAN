4593bf8dc0077a074f0b85a7ecd14717608154a4
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-02-22 16:22:57 -0600

    Update getprefix.py

--- upcean/getprefix/getprefix.py
+++ upcean/getprefix/getprefix.py
@@ -409,6 +409,12 @@ def get_goodwill_upca_barcode_info(upc, infotype=None):
   gw_item_color = None;
  price_matches = re.findall("^(\d{3})(\d{2})", upc_matches[1]);
  price_matches = price_matches[0];
+ if(price_matches[0]=="699" and gw_item_type=="Mystery Dozen Deal"):
+  gw_item_type = "Mystery 1/2 Dozen Deal";
+ elif(price_matches[0]=="999" and gw_item_type=="Mystery Dozen Deal"):
+  gw_item_type = "Mystery Dozen Deal";
+ else
+  gw_item_type = gw_item_type;
  price_alt = str(price_matches[0].lstrip('0'))+price_matches[1];
  formated_price = price_matches[0]+"."+price_matches[1];
  formated_price_alt = str(price_matches[0].lstrip('0'))+"."+price_matches[1];
