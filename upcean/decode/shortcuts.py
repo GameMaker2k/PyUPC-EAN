cf2960ec0537071b0104446ed4040bf5b4b6e6c9
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-03-16 23:50:02 -0500

    Create shortcuts.py

---
+++ upcean/decode/shortcuts.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+'''
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+    Copyright 2011-2023 Game Maker 2k - https://github.com/GameMaker2k
+    Copyright 2011-2023 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+    $FileInfo: shortcuts.py - Last Update: 3/9/2023 Ver. 2.7.27 RC 1 - Author: cooldude2k $
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals;
+import upcean.barcodes.barcode, upcean.support;
+
+''' // Shortcut Codes by Kazuki Przyborowski '''
+def decode_barcode(bctype,infile,resize=1,barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(bctype not in upcean.support.supported_barcodes("tuple")):
+  return False;
+ if(hasattr(upcean.decode.barcode, "create_"+bctype+"_barcode") and callable(getattr(upcean.decode.barcode, "decode_"+bctype+"_barcode"))):
+  return getattr(upcean.decode.barcode, "decode_"+bctype+"_barcode")(infile,resize,barheight,barwidth,textxy,barcolor,imageoutlib);
+ if(not hasattr(upcean.decode.barcode, "decode_"+bctype+"_barcode") or not callable(getattr(upcean.decode.barcode, "decode_"+bctype+"_barcode"))):
+  return False;
+ return False;
+
+def validate_decode_barcode(bctype,infile="./barcode.png",resize=1,barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(bctype not in upcean.support.supported_barcodes("tuple")):
+  return False;
+ if(bctype=="upca" or bctype=="goodwill" or bctype=="upce" or bctype=="ean13" or bctype=="ean" or bctype=="itf" or bctype=="itf14"):
+  if(hasattr(upcean.decode.barcode, "validate_decode_"+bctype+"_barcode") and callable(getattr(upcean.decode.barcode, "validate_decode_"+bctype+"_barcode"))):
+   return getattr(upcean.decode.barcode, "validate_decode_"+bctype+"_barcode")(infile,resize,barheight,barwidth,textxy,barcolor,imageoutlib);
+  if(not hasattr(upcean.decode.barcode, "validate_decode_"+bctype+"_barcode") or not callable(getattr(upcean.decode.barcode, "validate_decode_"+bctype+"_barcode"))):
+   return False;
+  return False;
+ if(bctype!="upca" and bctype!="upce" and bctype!="ean13" and bctype!="ean8"):
+  return decode_barcode(bctype,infile,resize,barheight,barwidth,textxy,barcolor,imageoutlib);
+
+def fix_decode_barcode(bctype,infile="./barcode.png",resize=1,barheight=(48, 54),barwidth=1,textxy=(1, 1, 1),barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(bctype not in upcean.support.supported_barcodes("tuple")):
+  return False;
+ if(bctype=="upca" or bctype=="upce" or bctype=="ean13" or bctype=="ean8"):
+  if(hasattr(upcean.decode.barcode, "fix_decode_"+bctype+"_barcode") and callable(getattr(upcean.decode.barcode, "fix_decode_"+bctype+"_barcode"))):
+   return getattr(upcean.decode.barcode, "fix_decode_"+bctype+"_barcode")(infile,resize,barheight,barwidth,textxy,barcolor,imageoutlib);
+  if(not hasattr(upcean.decode.barcode, "fix_decode_"+bctype+"_barcode") or not callable(getattr(upcean.decode.barcode, "fix_decode_"+bctype+"_barcode"))):
+   return False;
+  return False;
+ if(bctype!="upca" and bctype!="goodwill" and bctype!="upce" and bctype!="ean13" and bctype!="ean8"):
+  return decode_barcode(bctype,infile,resize,barheight,barwidth,textxy,barcolor,imageoutlib);
