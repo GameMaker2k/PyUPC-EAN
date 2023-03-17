ba5eac9c59d8afdb0214aebbda189983c0d00c4d
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-03-16 22:20:49 -0500

    Update ean13.py

--- upcean/decode/ean13.py
+++ upcean/decode/ean13.py
@@ -16,15 +16,25 @@
 
 from PIL import Image;
 
-def decode_ean13_barcode(infile="./upca.png",resize=1,hideinfo=(False, False, False),barheight=(48, 54),barwidth=1,barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+def decode_ean13_barcode(infile="./upca.png",resize=1,barheight=(48, 54),barwidth=1,barcolor=((0, 0, 0), (0, 0, 0), (255, 255, 255)), imageoutlib="pillow"):
+ if(not re.findall("^([0-9]*[\.]?[0-9])", str(resize)) or int(resize) < 1):
+  resize = 1;
  if(isinstance(infile, Image.Image)):
   upc_img = infile;
  else:
   try:
    infile.seek(0);
-   upc_img = Image.open(infile);
+   try:
+    upc_img = Image.open(infile);
+   except UnidentifiedImageError:
+    upc_img = Image.frombytes(infile.read());
   except AttributeError:
-   upc_img = Image.open(infile);
+   try:
+    upc_img = Image.open(infile);
+   except UnidentifiedImageError:
+    prefile = open(infile, "rb");
+    upc_img = Image.frombytes(prefile.read());
+    profile.close();
  barsize = barwidth * int(resize);
  starty = int(upc_img.size[1] / 2);
  fist_number_dict = { 'LLLLLL': "0", 'LLGLGG': "1", 'LLGGLG': "2", 'LLGGGL': "3", 'LGLLGG': "4", 'LGGLLG': "5", 'LGGGLL': "6", 'LGLGLG': "7", 'LGLGGL': "8", 'LGGLGL': "9" };
