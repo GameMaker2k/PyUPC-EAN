151dd5dc289db1536dab88efd245f86983c688a8
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-03-22 17:37:23 -0500

    Add files via upload

---
+++ upcean/encode/shortcuts.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2011-2023 Game Maker 2k - https://github.com/GameMaker2k
+    Copyright 2011-2023 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+
+    $FileInfo: shortcuts.py - Last Update: 3/9/2023 Ver. 2.7.27 RC 1 - Author: cooldude2k $
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals;
+import upcean.barcodes.barcode, upcean.xml.files, upcean.support;
+
+from upcean.barcodes.shortcuts import *;
