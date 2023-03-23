9dc9aa7b4fa945af5300124c48cf17f4904fd92f
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-03-23 18:11:26 -0500

    Updating version for next release.

--- examples/upc-xml.py
+++ examples/upc-xml.py
@@ -13,7 +13,7 @@
     Copyright 2011-2023 Game Maker 2k - https://github.com/GameMaker2k
     Copyright 2011-2023 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
 
-    $FileInfo: upc-xml.py - Last Update: 3/23/2023 Ver. 2.8.3 RC 1  - Author: cooldude2k $
+    $FileInfo: upc-xml.py - Last Update: 3/23/2023 Ver. 2.8.4 RC 1  - Author: cooldude2k $
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals;
