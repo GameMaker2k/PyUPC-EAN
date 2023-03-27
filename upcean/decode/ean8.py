5a8ee3fd05865fbac36daefe5a1aa5f94cc79fbc
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-03-26 20:35:24 -0500

    Update ean8.py

--- upcean/decode/ean8.py
+++ upcean/decode/ean8.py
@@ -69,7 +69,7 @@ def decode_ean8_barcode(infile="./ean8.png",resize=1,barheight=(48, 54),barwidth
      return False;
     icount = 0;
     imaxc = 3;
-    while(icount < imaxi):
+    while(icount < imaxc):
      curpixelist.append(upc_img.getpixel((inprestartx+(icount * (barwidth * int(resize))), starty)));
      icount += 1;
     inprestartx += (3 + 28) * (barwidth * int(resize));
@@ -78,7 +78,7 @@ def decode_ean8_barcode(infile="./ean8.png",resize=1,barheight=(48, 54),barwidth
      return False;
     icount = 0;
     imaxc = 5;
-    while(icount < imaxi):
+    while(icount < imaxc):
      curpixelist.append(upc_img.getpixel((inprestartx+(icount * (barwidth * int(resize))), starty)));
      icount += 1;
     inprestartx += (5 + 28) * (barwidth * int(resize));
@@ -86,7 +86,7 @@ def decode_ean8_barcode(infile="./ean8.png",resize=1,barheight=(48, 54),barwidth
      return False;
     icount = 0;
     imaxc = 3;
-    while(icount < imaxi):
+    while(icount < imaxc):
      curpixelist.append(upc_img.getpixel((inprestartx+(icount * (barwidth * int(resize))), starty)));
      icount += 1;
     if((curpixelist[0]==barcolor[0] and curpixelist[1]==barcolor[2] and curpixelist[2]==barcolor[0]) and (curpixelist[3]==barcolor[2] and curpixelist[4]==barcolor[0] and curpixelist[5]==barcolor[2] and curpixelist[6]==barcolor[0] and curpixelist[7]==barcolor[2]) and (curpixelist[8]==barcolor[0] and curpixelist[9]==barcolor[2] and curpixelist[10]==barcolor[0])):
