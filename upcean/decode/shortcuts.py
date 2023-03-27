be322e1f237e0a400511b9b4c8b2b01683eb100b
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-03-27 12:20:00 -0500

    Add files via upload

--- upcean/decode/shortcuts.py
+++ upcean/decode/shortcuts.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 '''
     This program is free software; you can redistribute it and/or modify
     it under the terms of the Revised BSD License.
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     Revised BSD License for more details.
+
     Copyright 2011-2023 Game Maker 2k - https://github.com/GameMaker2k
     Copyright 2011-2023 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+
     $FileInfo: shortcuts.py - Last Update: 3/26/2023 Ver. 2.8.11 RC 1 - Author: cooldude2k $
 '''
 
