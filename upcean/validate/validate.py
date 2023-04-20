35b2a0b98926792420f57e9ee5d4a71c250b592b
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-04-19 19:24:23 -0500

    Update validate.py

--- upcean/validate/validate.py
+++ upcean/validate/validate.py
@@ -811,7 +811,7 @@ def get_code39_checksum(upc, getmod="43"):
 // Source: http://www.barcodeisland.com/code93.phtml
 // Source: http://en.wikipedia.org/wiki/Code_93
 '''
-def get_code93_checksum(upc):
+def get_code93_alt_checksum(upc):
  if(len(upc) < 1):
   return False;
  if(not re.findall("([0-9a-zA-Z\-\.\$\/\+% ]+)", upc)):
@@ -847,6 +847,37 @@ def get_code93_checksum(upc):
   UPC_Count += 1;
   UPC_Weight += 1;
  CheckSum = str(CheckSum)+str(Code93Array[UPC_Sum % 47]);
+ return str(CheckSum);
+
+'''
+// Code 93
+// Source: http://www.barcodeisland.com/code93.phtml
+// Source: http://en.wikipedia.org/wiki/Code_93
+'''
+def get_code93_checksum(upc):
+ if(len(upc) < 1):
+  return False;
+ if(not re.findall("([0-9a-zA-Z\-\.\$\/\+% ]+)", upc)):
+  return False;
+ upc = upc.upper();
+ upc_matches = list(upc);
+ if(len(upc_matches)<=0):
+  return False;
+ Code93Array = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9", 10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F", 16: "G", 17: "H", 18: "I", 19: "J", 20: "K", 21: "L", 22: "M", 23: "N", 24: "O", 25: "P", 26: "Q", 27: "R", 28: "S", 29: "T", 30: "U", 31: "V", 32: "W", 33: "X", 34: "Y", 35: "Z", 36: "-", 37: ".", 38: " ", 39: "$", 40: "/", 41: "+", 42: "%", 43: "($)", 44: "(%)", 45: "(/)", 46: "(+)"};
+ Code93Values = dict(zip(Code93Array.values(),Code93Array));
+ upc_reverse = list(upc_matches);
+ upc_reverse.reverse();
+ upc_print = list(upc_matches);
+ UPC_Count = 0;
+ UPC_Weight = 1;
+ UPC_Sum = 0;
+ while (UPC_Count < len(upc_reverse)):
+  if(UPC_Weight>20):
+   UPC_Weight = 1;
+  UPC_Sum += (UPC_Weight * Code93Values[str(upc_reverse[UPC_Count])]);
+  UPC_Count += 1;
+  UPC_Weight += 1;
+ CheckSum = str(Code93Array[UPC_Sum % 47]);
  return str(CheckSum);
 
 '''
