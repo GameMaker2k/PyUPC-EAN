616bd660734f643644c64761f847d61ace46885d
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2023-12-02 16:33:20 -0600

    Update __init__.py

--- upcean/xml/__init__.py
+++ upcean/xml/__init__.py
@@ -17,21 +17,41 @@
 from __future__ import absolute_import, division, print_function, unicode_literals;
 import os;
 
+implib = False
+pkgres = False;
 try:
- import pkg_resources;
- pkgres = True;
+ import importlib.resources;
+ implib = True;
 except ImportError:
- pkgres = False;
+ implib = False
+ try:
+  import pkg_resources;
+  pkgres = True;
+ except ImportError:
+  pkgres = False;
 
-if(pkgres):
+if(implib):
+ barcodedtd = os.path.join(importlib.resources.files(__name__), "barcodes.dtd");
+ barcodexsl = os.path.join(importlib.resources.files(__name__), "barcodes.xsl");
+ barcodexsd = os.path.join(importlib.resources.files(__name__), "barcodes.xsd");
+ barcoderng = os.path.join(importlib.resources.files(__name__), "barcodes.rng");
+ barcodernc = os.path.join(importlib.resources.files(__name__), "barcodes.rnc");
+ bcxmlpath = os.path.dirname(barcodedtd);
+elif(pkgres):
  barcodedtd = pkg_resources.resource_filename(__name__, "barcodes.dtd");
  barcodexsl = pkg_resources.resource_filename(__name__, "barcodes.xsl");
  barcodexsd = pkg_resources.resource_filename(__name__, "barcodes.xsd");
  barcoderng = pkg_resources.resource_filename(__name__, "barcodes.rng");
  barcodernc = pkg_resources.resource_filename(__name__, "barcodes.rnc");
  bcxmlpath = os.path.dirname(barcodedtd);
-
-if(not pkgres):
+elif(not pkgres):
+ barcodedtd = os.path.dirname(__file__)+os.sep+"barcodes.dtd";
+ barcodexsl = os.path.dirname(__file__)+os.sep+"barcodes.xsl";
+ barcodexsd = os.path.dirname(__file__)+os.sep+"barcodes.xsd";
+ barcoderng = os.path.dirname(__file__)+os.sep+"barcodes.rng";
+ barcodernc = os.path.dirname(__file__)+os.sep+"barcodes.rnc";
+ bcxmlpath = os.path.dirname(barcodedtd);
+else:
  barcodedtd = os.path.dirname(__file__)+os.sep+"barcodes.dtd";
  barcodexsl = os.path.dirname(__file__)+os.sep+"barcodes.xsl";
  barcodexsd = os.path.dirname(__file__)+os.sep+"barcodes.xsd";
