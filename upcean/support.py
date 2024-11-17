d90828d4b21339f729bd1b3341015a879e7f1007
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-11-17 16:28:43 -0600

    Update support.py

--- upcean/support.py
+++ upcean/support.py
@@ -17,6 +17,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals, generators, with_statement, nested_scopes
 import os
 import platform
+import inspect
 try:
     import configparser
 except ImportError:
