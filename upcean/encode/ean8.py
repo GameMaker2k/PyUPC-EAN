bccbdc573b8ded20dc28c8d7afcbec0c8ff0a945
Kazuki Suzuki Przyborowski <kazuki.przyborowski@gmail.com>
2024-11-19 17:48:35 -0600

    Small update

--- upcean/encode/ean8.py
+++ upcean/encode/ean8.py
@@ -53,18 +53,6 @@ cv2support = upcean.support.check_for_cv2()
 skimagesupport = upcean.support.check_for_skimage()
 imagelibsupport = upcean.support.imagelibsupport
 defaultdraw = upcean.support.defaultdraw
-if(pilsupport or pillowsupport):
-    import upcean.predraw.prepil
-if(cairosupport):
-    import upcean.predraw.precairo
-if(qahirahsupport):
-    import upcean.predraw.preqahirah
-if(svgwritesupport):
-    import upcean.predraw.presvgwrite
-if(wandsupport):
-    import upcean.predraw.prewand
-if(magicksupport):
-    import upcean.predraw.premagick
 
 
 def get_ean8_barcode_size(upc, resize=1, shiftxy=(0, 0), barheight=(48, 54), barwidth=(1, 1)):
